b76f846b561854332ca6e7739ee03842d357c1ac
209450 <209450@gmail.com>
2019-03-13 19:01:55 +0100

    Refaktoryzacja wywołań metod klasy OfferItem w klasie Offer

--- src/main/java/pl/com/bottega/ecommerce/sales/domain/offer/Offer.java
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/offer/Offer.java
@@ -63,7 +63,7 @@ public class Offer {
         }
 
         for (OfferItem item : availableItems) {
-            OfferItem sameItem = seenOffer.findItem(item.getProductId());
+            OfferItem sameItem = seenOffer.findItem(item.getProduct().getProductId());
             if (sameItem == null) {
                 return false;
             }
@@ -77,7 +77,7 @@ public class Offer {
 
     private OfferItem findItem(String productId) {
         for (OfferItem item : availableItems) {
-            if (item.getProductId().equals(productId)) {
+            if (item.getProduct().getProductId().equals(productId)) {
                 return item;
             }
         }
